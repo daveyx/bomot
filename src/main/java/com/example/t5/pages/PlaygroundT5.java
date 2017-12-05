02a9b2fa7e65ece4e30d192fb3f002b5dcd890f1
daveyx <d.rehle@gmail.com>
2017-12-05 12:03:55 +0100

    added ajax zone for future use

--- src/main/java/com/example/t5/pages/PlaygroundT5.java
+++ src/main/java/com/example/t5/pages/PlaygroundT5.java
@@ -1,8 +1,20 @@
 package com.example.t5.pages;
 
 import org.apache.shiro.authz.annotation.RequiresAuthentication;
+import org.apache.tapestry5.Block;
+import org.apache.tapestry5.ioc.annotations.Inject;
+import org.apache.tapestry5.services.ajax.AjaxResponseRenderer;
 
 @RequiresAuthentication
 public class PlaygroundT5 {
 
+	@Inject
+	private AjaxResponseRenderer ajaxResponseRenderer;
+
+	@Inject
+	private Block block;
+
+	void onAjax() {
+		ajaxResponseRenderer.addRender("middlezone", block);
+	}
 }
