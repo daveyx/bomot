0a584de4cc3c602e4e504ce8dd21e57423998d20
daveyx <d.rehle@gmail.com>
2017-12-05 04:04:56 +0100

    fixed inheritance

--- src/main/java/com/example/persistence/model/PAbstractEntity.java
+++ src/main/java/com/example/persistence/model/PAbstractEntity.java
@@ -1,14 +1,14 @@
 package com.example.persistence.model;
 
-import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 
 import lombok.Data;
 
 @Data
-@Entity
+@MappedSuperclass
 public abstract class PAbstractEntity {
 
 	@Id
